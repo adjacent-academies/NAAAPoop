a471a3e76af702aa49839a5e87d018f6bd0813f7
Alexander Idonije <alidonije@davidson.edu>
2019-07-25 16:57:10 -0700

    Update the style of the website

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,7 @@
 console.log("hello from the console")
+
+// Home button 
+$("#home button").on("click", () => {
+    alert("");  
+    console.log("onclick")                            // what does the homebutton do?
+   });
