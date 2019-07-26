3898404af6c10c1f382bcad60d562b263f708ee5
Alexander Idonije <alidonije@davidson.edu>
2019-07-26 14:06:34 -0700

    page layout updates

--- routes/map.js
+++ routes/map.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('map', { title: 'Fecal Finder' });
+router.get("/", function(req, res, next) {
+  res.render("map", { title: "Fecal Finder" });
 });
 
 module.exports = router;
