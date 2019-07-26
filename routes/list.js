d2353ee5ca30e9bbc8266735d1b0ea4e8deffc10
Alexander Idonije <alidonije@davidson.edu>
2019-07-26 11:55:41 -0700

    - Add locations to the maps/list page - reorganize page content

--- routes/list.js
+++ routes/list.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+const SFBathroomFinder = require("../public/SFBathroomFinder.json");
+const locations = SFBathroomFinder.features
+
+
 router.get('/', function(req, res, next) {
-  res.render('list', { title: 'List View for the Bathroom Mapper' });
+  res.render('list', { title: 'List View for the Bathroom Mapper', locations});
 });
 
 module.exports = router;
